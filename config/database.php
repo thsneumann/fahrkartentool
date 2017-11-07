9376667f40b210e1bb3e212aeba572e0e52cfdb4
Thomas Neumann <thsneumann@gmail.com>
2017-11-07 22:40:06 +0100

    fix key too long error

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', getenv('DB_HOST')),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', getenv('DB_DATABASE')),
+            'username' => env('DB_USERNAME', getenv('DB_USERNAME')),
+            'password' => env('DB_PASSWORD', getenv('DB_PASSWORD')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
