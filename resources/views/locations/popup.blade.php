994437e596556554559a400ff606c3385553bc84
Thomas Neumann <thsneumann@gmail.com>
2017-11-30 20:29:39 +0100

    map styling; add edit button to ticket thumbs in map popup

--- resources/views/locations/popup.blade.php
+++ resources/views/locations/popup.blade.php
@@ -6,7 +6,10 @@
         <ul class="ticket-list">
         @foreach ($podTickets as $ticket)
             <li>
-            @include('partials.ticket-thumb', ['ticket' => $ticket])
+                @include('partials.ticket-thumb', ['ticket' => $ticket])
+                <a href="{{ route('tickets.edit', ['id' => $ticket->id]) }}" class="btn btn-sm btn-primary mr-2" role="button">
+                    <i class="fa fa-pencil" aria-hidden="true"></i>
+                </a>
             </li>
         @endforeach
         </ul>
@@ -17,8 +20,15 @@
         <ul class="ticket-list">
         @foreach ($destTickets as $ticket)
             <li>
-            @include('partials.ticket-thumb', ['ticket' => $ticket])
+                @include('partials.ticket-thumb', ['ticket' => $ticket])
+                <a href="{{ route('tickets.edit', ['id' => $ticket->id]) }}" class="btn btn-sm btn-primary mr-2" role="button">
+                    <i class="fa fa-pencil" aria-hidden="true"></i>
+                </a>
             </li>
         @endforeach
+    @endif
+
+    @if (count($podTickets) == 0 && count($destTickets) == 0)
+        <p><em>Diesem Ort wurden noch keine Tickets zugeordnet</em></p>
     @endif
 </div>
