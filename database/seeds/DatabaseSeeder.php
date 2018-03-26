0766167e4c87b359c5db3cf56ea5525d2431a407
Thomas Neumann <vagrant@homestead>
2018-03-26 19:22:04 +0000

    add location seeder with random locations generated by faker

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(VehicleClassesTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(TicketsTableSeeder::class);
+        $this->call(LocationsTableSeeder::class);
     }
 }
---
+++ database/seeds/LocationsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Location;
+
+class LocationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker\Generator $faker)
+    {
+        $faker = Faker\Factory::create();
+
+        $max = 1000;
+
+        for ($i = 0; $i < $max; $i++) {
+            $location = new Location();
+            $location->name = $faker->city;
+            $location->longname = $location->name;
+            $location->lat = $faker->latitude(40, 60);
+            $location->lng = $faker->longitude(-10,40);
+            $location->save();
+        }
+    }
+}
