ac5fb25bb9b191daa05b1e234217b0b92abd59b3
Thomas Neumann <thsneumann@gmail.com>
2018-04-16 18:34:35 +0200

    make intro page responsive

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -5,27 +5,37 @@
 
 @section('content')
   <div class="container-fluid">
-    <div class="d-flex mb-5">
-      <h1 class="text-uppercase font-weight-bold">Los geht&apos;s!</h1>
-      <ul class="d-flex ml-auto text-uppercase text-size-1">
-        <li class="mr-4">
-          <a href="{{ route('about') }}">Info</a>
-        </li>
-        <li>
-          <a href="#">Impressum</a>
-        </li>
-      </ul>   
+    <div class="row mb-3">
+      <div class="col-sm-6">
+        <h1 class="text-uppercase font-weight-bold">Los geht&apos;s!</h1>
+      </div>
+      <div class="col-sm-6">
+        <ul class="d-flex text-uppercase text-size-1">
+            <li class="ml-sm-auto mr-4">
+              <a href="{{ route('about') }}" class="d-flex align-items-center">
+                Info
+                <img src="img/pfeil_rechts.png" width="20" class="ml-2" alt="Pfeil nach rechts.">
+              </a>
+            </li>
+            <li>
+              <a href="#" class="d-flex align-items-center">
+                Impressum
+                <img src="img/pfeil_rechts.png" width="20" class="ml-2" alt="Pfeil nach rechts.">
+              </a>
+            </li>
+          </ul>   
+      </div>
     </div>
     
     <div class="row mb-3">
-      <div class="col-sm-5">
+      <div class="col-lg-7">
         <p class="mb-2">Wie schön, dass Sie sich an der Erschließung der historischen Fahrkartensammlung des Deutschen Technikmuseums in Berlin beteiligen wollen.</p>
         <p class="mb-2"><strong>Und so funktioniert das Ganze:</strong> Sie können auf zwei Arten an den Fahrkarten arbeiten: Im ersten Modus schreiben Sie bitte die Informationen von den Karten in die passenden Felder. Im zweiten Modus überprüfen Sie die Einträge, die bereits gemacht wurden.</p>
         <p class="mb-2"><strong>Und für alle Ihre Aktionen sammeln Sie PUNKTE!</strong> Schaffen Sie es bis zum 31. Dezember 2018 ganz nach oben in der Bestenliste? Die ersten Drei nach Punkten dürfen sich auf ein Dankeschön von der Stiftung Deutsches Technikmuseum Berlin freuen!</p>
         <p class="mb-2"><strong>Im Menü Erkunden können Sie mitverfolgen, wie die Daten wachsen.</strong> Dort sind die Infos von allen bereits bearbeiteten Tickets abrufbar – auf einer Karte oder in verschiedenen Listen.</p>
         <p><strong>Und noch besser:</strong> Alle Einträge, die gesammelt wurden, können per JSON/CSV-Export runterladen werden. Bitte nutzen oder teilen Sie die Daten. Es gilt die Creative Commons Lizenz CC0.</p> 
       </div>
-      <div class="col-sm-4 offset-sm-3">
+      <div class="col-lg-4 offset-lg-1">
         @include('partials/image-grid')
       </div>
     </div>
