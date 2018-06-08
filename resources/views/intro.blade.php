8b13ccd20c7d3c8a3cdb8b965a82693edf135978
Thomas Neumann <thsneumann@gmail.com>
2018-06-08 12:36:57 +0200

    fix validation errors, code formatting

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -11,25 +11,36 @@
       </div>
       <div class="col-sm-6">
         <nav class="navbar navbar-light">
-            <ul class="navbar-nav">
-              <li class="nav-item text-sm-right">
-                <a class="nav-link text-size-2" href="{{ route('about') }}">
-                  <i class="fa fa-info-circle" aria-hidden="true"></i>
-                  Info
-                </a>
-              </li>
-            </ul>
-          </div>
+          <ul class="navbar-nav">
+            <li class="nav-item text-sm-right">
+              <a class="nav-link text-size-2" href="{{ route('about') }}">
+                <i class="fa fa-info-circle" aria-hidden="true"></i>
+                Info
+              </a>
+            </li>
+          </ul>
         </nav>
+      </div>
     </div>
-    
+
     <div class="row mb-3">
       <div class="col-lg-7">
-        <p class="mb-2">Wie schön, dass Sie sich an der Erschließung der historischen Fahrkartensammlung des Deutschen Technikmuseums in Berlin beteiligen wollen.</p>
-        <p class="mb-2"><strong>Und so funktioniert das Ganze:</strong> Sie können auf zwei Arten an den Fahrkarten arbeiten: Im ersten Modus schreiben Sie bitte die Informationen von den Karten in die passenden Felder. Im zweiten Modus überprüfen Sie die Einträge, die bereits gemacht wurden.</p>
-        <p class="mb-2"><strong>Und für alle Ihre Aktionen sammeln Sie PUNKTE!</strong> Schaffen Sie es bis zum 31. Dezember 2018 ganz nach oben in der Bestenliste? Die ersten Drei nach Punkten dürfen sich auf ein Dankeschön von der Stiftung Deutsches Technikmuseum Berlin freuen!</p>
-        <p class="mb-2"><strong>Im Menü Erkunden können Sie mitverfolgen, wie die Daten wachsen.</strong> Dort sind die Infos von allen bereits bearbeiteten Tickets abrufbar – auf einer Karte oder in verschiedenen Listen.</p>
-        <p><strong>Und noch besser:</strong> Alle Einträge, die gesammelt wurden, können per JSON/CSV-Export runterladen werden. Bitte nutzen oder teilen Sie die Daten. Es gilt die Creative Commons Lizenz CC0.</p> 
+        <p class="mb-2">Wie schön, dass Sie sich an der Erschließung der historischen Fahrkartensammlung des Deutschen Technikmuseums in
+          Berlin beteiligen wollen.</p>
+        <p class="mb-2">
+          <strong>Und so funktioniert das Ganze:</strong> Sie können auf zwei Arten an den Fahrkarten arbeiten: Im ersten Modus schreiben
+          Sie bitte die Informationen von den Karten in die passenden Felder. Im zweiten Modus überprüfen Sie die Einträge,
+          die bereits gemacht wurden.</p>
+        <p class="mb-2">
+          <strong>Und für alle Ihre Aktionen sammeln Sie PUNKTE!</strong> Schaffen Sie es bis zum 31. Dezember 2018 ganz nach oben
+          in der Bestenliste? Die ersten Drei nach Punkten dürfen sich auf ein Dankeschön von der Stiftung Deutsches Technikmuseum
+          Berlin freuen!</p>
+        <p class="mb-2">
+          <strong>Im Menü Erkunden können Sie mitverfolgen, wie die Daten wachsen.</strong> Dort sind die Infos von allen bereits
+          bearbeiteten Tickets abrufbar – auf einer Karte oder in verschiedenen Listen.</p>
+        <p>
+          <strong>Und noch besser:</strong> Alle Einträge, die gesammelt wurden, können per JSON/CSV-Export runterladen werden. Bitte
+          nutzen oder teilen Sie die Daten. Es gilt die Creative Commons Lizenz CC0.</p>
       </div>
       <div class="col-lg-4 offset-lg-1">
         @include('partials/image-grid')
@@ -40,12 +51,13 @@
       <div class="col-sm-8">
         <div class="footer-links">
           <a href="{{ route('edit.index') }}" class="d-flex align-items-center font-weight-bold text-uppercase text-size-3">
-              Bearbeiten
-              <img src="{{ asset('img/pfeil_rechts.png') }}" alt="Pfeil nach rechts." class="ml-auto">
-          </a><br>
+            Bearbeiten
+            <img src="{{ asset('img/pfeil_rechts.png') }}" alt="Pfeil nach rechts." class="ml-auto">
+          </a>
+          <br>
           <a href="{{ route('explore.index') }}" class="d-flex align-items-center font-weight-bold text-uppercase text-size-3">
-              Erkunden
-              <img src="{{ asset('img/pfeil_rechts.png') }}" alt="Pfeil nach rechts." class="ml-auto">
+            Erkunden
+            <img src="{{ asset('img/pfeil_rechts.png') }}" alt="Pfeil nach rechts." class="ml-auto">
           </a>
         </div>
       </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,57 +5,56 @@
 
 @section('content')
 
-<div class="container-fluid d-flex flex-column">
-    <div class="row mb-3">
-        <div class="col-lg-6 col-xl-5">
-            <p class="text-size-3 text-uppercase font-weight-bold">Herzlich Willkommen!</p>
+    <div class="container-fluid d-flex flex-column">
+        <div class="row mb-3">
+            <div class="col-lg-6 col-xl-5">
+                <p class="text-size-3 text-uppercase font-weight-bold">Herzlich Willkommen!</p>
 
-            <h1 class="text-size-3 text-uppercase font-weight-bold mb-4">Fritz-Fahrkartentool</h1>
-            <div class="text-size-1 font-weight-bold">
-                <p>Die historische Fahrkartensammlung<br>
-                des Deutschen Technikmuseums<br>
-                … gesammelt von Opernsänger Fritz Hellmuth,<br>
-                … bearbeitet von der Crowd!<br>
-                </p>
-                <p>
-                Bergen Sie mit uns den Schatz von 120.000 internationalen Tickets aus der Zeit der Entstehung des Personenverkehrs Mitte des 19. Jahrhunderts bis 1925.
-                </p>
+                <h1 class="text-size-3 text-uppercase font-weight-bold mb-4">Fritz-Fahrkartentool</h1>
+                <div class="text-size-1 font-weight-bold">
+                    <p>Die historische Fahrkartensammlung
+                        <br> des Deutschen Technikmuseums
+                        <br> … gesammelt von Opernsänger Fritz Hellmuth,
+                        <br> … bearbeitet von der Crowd!
+                        <br>
+                    </p>
+                    <p>
+                        Bergen Sie mit uns den Schatz von 120.000 internationalen Tickets aus der Zeit der Entstehung des Personenverkehrs Mitte
+                        des 19. Jahrhunderts bis 1925.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-5 offset-lg-1 col-xl-4 offset-xl-3">
+                @include('partials/image-grid')
             </div>
         </div>
-        <div class="col-lg-5 offset-lg-1 col-xl-4 offset-xl-3">
-            @include('partials/image-grid')
-        </div>
-    </div>
 
-    <div class="row align-items-end mt-auto">
-        <div class="col-md-7 col-lg-8 mb-3 mb-md-0">
-            <img src="{{ asset('img/raute_technikmuseum.png') }}" alt="Logo Stiftung Deutsches Technikmuseum" class="mb-3">
-            <div class="d-flex align-items-start">
-                <a href="{{ route('intro') }}" class="d-flex align-items-center font-weight-bold text-uppercase text-size-3 mb-2 mb-sm-0">
-                    Los geht&apos;s!
-                    <img src="{{ asset('img/pfeil_rechts.png') }}" alt="Pfeil nach rechts." class="ml-4">
+        <div class="row align-items-end mt-auto">
+            <div class="col-md-7 col-lg-8 mb-3 mb-md-0">
+                <img src="{{ asset('img/raute_technikmuseum.png') }}" alt="Logo Stiftung Deutsches Technikmuseum" class="mb-3">
+                <div class="d-flex align-items-start">
+                    <a href="{{ route('intro') }}" class="d-flex align-items-center font-weight-bold text-uppercase text-size-3 mb-2 mb-sm-0">
+                        Los geht&apos;s!
+                        <img src="{{ asset('img/pfeil_rechts.png') }}" alt="Pfeil nach rechts." class="ml-4">
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-5 col-lg-4">
+                <a href="#intro-video" class="video-preview" data-fancybox data-small-btn="true">
+                    <figure class="mb-0">
+                        <img src="{{ asset('img/video-vorschau.jpg') }}" alt="Vorschaubild Erklärvideo.">
+                        <figcaption class="d-flex align-items-center caption text-size-1 text-uppercase">
+                            Zum Video über Fritz
+                            <img src="{{ asset('img/pfeil_rechts.png') }}" width="30" alt="Pfeil nach rechts." class="ml-2">
+                        </figcaption>
+                    </figure>
                 </a>
             </div>
         </div>
-        <div class="col-md-5 col-lg-4">
-            <a href="#intro-video" class="video-preview" data-fancybox data-small-btn="true">
-                <figure class="mb-0">
-                    <img src="{{ asset('img/video-vorschau.jpg') }}" alt="Vorschaubild Erklärvideo.">
-                    <figcaption class="d-flex align-items-center caption text-size-1 text-uppercase">
-                        Zum Video über Fritz
-                        <img src="{{ asset('img/pfeil_rechts.png') }}" width="30" alt="Pfeil nach rechts." class="ml-2">
-                    </figcaption>
-                </figure>
-            </a>
-        </div>
     </div>
-</div>
 
-<video id="intro-video" controls style="display: none">
-    <source src="{{ asset('video/intro.mp4') }}" type="video/mp4">
-    Ihr Browser unterstützt keine HTML5-Videos.
-</video>
-
-</div>
+    <video id="intro-video" controls style="display: none">
+        <source src="{{ asset('video/intro.mp4') }}" type="video/mp4"> Ihr Browser unterstützt keine HTML5-Videos.
+    </video>
 
 @endsection
