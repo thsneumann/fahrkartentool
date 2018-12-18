abf824d59c390b670de9f3c1cf3ae643e806bc14
Thomas Neumann <thsneumann@gmail.com>
2018-12-18 22:23:05 +0100

    add message and trophy icons to highscore page

--- resources/views/edit/highscore.blade.php
+++ resources/views/edit/highscore.blade.php
@@ -5,6 +5,11 @@
 <div class="container-fluid">
     <h2>Bestenliste</h2>
 
+    <div class="alert alert-success" role="alert">
+        <p>Danke für alle Unterstützer im Jahr 2018. Durch Ihre Hilfe konnten in nur wenigen Monaten beachtlich viele Karten verschlagwortet werden.</p>
+        <p>Unser besonderer Dank geht an Schwarze Jule Frost, snoor 33 und Max!</p>
+    </div>
+
     <table class="table">
         <thead>
             <th scope="col">#</th>
@@ -15,7 +20,12 @@
             @foreach ($users as $user)
             <tr>
                 <th scope="row">{{ $loop->iteration }}</th>
-                <td>{{ $user->name }}</td>
+                <td>
+                    {{ $user->name }}
+                    @if ($loop->iteration <= 3)
+                        <i class="fa fa-lg fa-trophy"></i>
+                    @endif
+                </td>
                 <td>{{ $user->points or 0 }}</td>
             </tr>
             @endforeach
